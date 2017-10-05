763f410f6b3af3bee6f31279ec5b5b85e248fced
PollenJain <paragjainpes@gmail.com>
2017-10-05 13:08:16 +0530

    adding grid view

---
+++ grid_view_slidenerd/app/src/test/java/com/example/paragjain/grid_view_slidenerd/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.paragjain.grid_view_slidenerd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
